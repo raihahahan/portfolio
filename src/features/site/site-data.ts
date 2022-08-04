353c8f43a5ecff2080cb5a1b1b98882c783cad1b
Muhd Raihan <muhdraihan1140@gmail.com>
2022-08-04 14:52:35 +0800

    changed footer links

--- src/features/site/site-data.ts
+++ src/features/site/site-data.ts
@@ -16,9 +16,18 @@ export default anchorData;
 
 export const footerData: { title: anchorTitles; anchorRoute: routes }[] = [
   {
+    title: "M.Raihan" as any,
+    anchorRoute: "/about",
+  },
+  {
+    title: "Github" as any,
+    anchorRoute: "https://www.github.com/raihahahan" as any,
+  },
+  {
     title: "mraihandev@gmail.com" as any,
     anchorRoute: "mailto:mraihandev@gmail.com" as any,
   },
+
   {
     title: "Privacy Policy",
     anchorRoute: "/privacy-policy",
