1aadd26bc6e0f64b1e00f6abe594abb2bd75e82e
raihahahan <mraihandev@gmail.com>
2026-03-16 10:25:09 +0800

    fix analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -32,7 +32,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
+            siteTitle,
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
@@ -54,6 +54,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <ReduxProvider>
           <SiteLayout>
             <Component {...pageProps} />
+            <Analytics />
           </SiteLayout>
         </ReduxProvider>
       </MantineProvider>
